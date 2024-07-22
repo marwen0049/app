c8df33e049d9563576122564c27914297cf8281e
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-07-22 23:20:13 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM entièrement chargé');
+    // Ajouter des fonctionnalités JavaScript ici si nécessaire
+});
