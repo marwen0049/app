1e22b5da2fd63ccd82a5b3fbacddfa8c7adb40f1
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-07-22 23:50:59 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
 // scripts.js
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM entièrement chargé');
-    // Ajouter des fonctionnalités JavaScript ici si nécessaire
+    const carousels = document.querySelectorAll('.carousel');
+    
+    carousels.forEach(carousel => {
+        let index = 0;
+        const items = carousel.querySelectorAll('iframe');
+        const totalItems = items.length;
+        const nextButton = carousel.querySelector('.next');
+        const prevButton = carousel.querySelector('.prev');
+        
+        nextButton.addEventListener('click', () => {
+            index = (index + 1) % totalItems;
+            updateCarousel();
+        });
+
+        prevButton.addEventListener('click', () => {
+            index = (index - 1 + totalItems) % totalItems;
+            updateCarousel();
+        });
+
+        function updateCarousel() {
+            const offset = -index * 100;
+            carousel.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+        }
+    });
 });
